96fe71236c30dd65c10db283a8e190a5ec2fe05b
simpleframework <cknet@126.com>
2014-05-04 17:25:37 +0800

    Signed-off-by: simpleframework <cknet@126.com>

--- src/main/java/net/simpleframework/module/news/NewsCategory.java
+++ src/main/java/net/simpleframework/module/news/NewsCategory.java
@@ -1,7 +1,11 @@
 package net.simpleframework.module.news;
 
+import java.util.Date;
+
+import net.simpleframework.ado.bean.IDateAwareBean;
 import net.simpleframework.ado.bean.IDomainBeanAware;
 import net.simpleframework.ado.db.common.EntityInterceptor;
+import net.simpleframework.common.ID;
 import net.simpleframework.module.common.content.AbstractCategoryBean;
 
 /**
@@ -11,13 +15,17 @@ import net.simpleframework.module.common.content.AbstractCategoryBean;
  *         http://www.simpleframework.net
  */
 @EntityInterceptor(listenerTypes = { "net.simpleframework.module.log.EntityDeleteLogAdapter" })
-public class NewsCategory extends AbstractCategoryBean implements IDomainBeanAware {
-
+public class NewsCategory extends AbstractCategoryBean implements IDomainBeanAware, IDateAwareBean {
 	/* 域，用在在线系统。默认为0 */
 	private int domain;
 
 	/* 查看模板 */
 	private int viewTemplate;
+
+	/* 创建人 */
+	private ID userId;
+	/* 创建日期 */
+	private Date createDate;
 
 	@Override
 	public int getDomain() {
@@ -35,6 +43,27 @@ public class NewsCategory extends AbstractCategoryBean implements IDomainBeanAwa
 
 	public void setViewTemplate(final int viewTemplate) {
 		this.viewTemplate = viewTemplate;
+	}
+
+	public ID getUserId() {
+		return userId;
+	}
+
+	public void setUserId(final ID userId) {
+		this.userId = userId;
+	}
+
+	@Override
+	public Date getCreateDate() {
+		if (createDate == null) {
+			createDate = new Date();
+		}
+		return createDate;
+	}
+
+	@Override
+	public void setCreateDate(final Date createDate) {
+		this.createDate = createDate;
 	}
 
 	private static final long serialVersionUID = -1520445282796635254L;
