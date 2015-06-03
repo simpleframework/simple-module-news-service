1bc033de176b89a6c526bd7eee1544c93848af8d
CHENKAN <CHENKAN@CHENKAN-PC>
2015-06-03 17:27:35 +0800

    Signed-off-by: CHENKAN <CHENKAN@CHENKAN-PC>

--- src/main/java/net/simpleframework/module/news/impl/NewsService.java
+++ src/main/java/net/simpleframework/module/news/impl/NewsService.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.util.Map;
 
 import net.simpleframework.ado.ColumnData;
-import net.simpleframework.ado.EFilterRelation;
 import net.simpleframework.ado.FilterItem;
 import net.simpleframework.ado.FilterItems;
 import net.simpleframework.ado.IParamsValue;
@@ -61,8 +60,7 @@ public class NewsService extends AbstractRecommendContentService<News> implement
 		if (status != null) {
 			filterItems.add(new FilterItem("status", status));
 		} else {
-			filterItems
-					.add(new FilterItem("status", EFilterRelation.not_equal, EContentStatus.delete));
+			filterItems.addNotEqual("status", EContentStatus.delete);
 		}
 
 		return queryByParams(filterItems, orderColumns);
