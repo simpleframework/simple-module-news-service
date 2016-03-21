4c70d248149874828819636a28ed8feb506fe451
CHENK <CHENK@CHENK-PC>
2016-03-21 18:53:53 +0800

    Signed-off-by: CHENK <CHENK@CHENK-PC>

---
+++ src/main/java/net/simpleframework/module/news/bean/NewsFee.java
@@ -0,0 +1,14 @@
+package net.simpleframework.module.news.bean;
+
+import net.simpleframework.ado.bean.AbstractIdBean;
+
+/**
+ * @author 陈侃(cknet@126.com, 13910090885)
+ *         https://github.com/simpleframework
+ *         http://www.simpleframework.net
+ */
+public class NewsFee extends AbstractIdBean {
+
+	// private
+	private static final long serialVersionUID = 6535341618367647105L;
+}
